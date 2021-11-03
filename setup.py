53bd57d8fe3ce6cea5fddea278a4e30f9299ed0b
dariongbc <dariongbc@gmail.com>
2021-11-03 16:40:40 +0100

    Update setup.py
    changed readme file directory for proper installation from link

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def read(*filenames, **kwargs):
             buf.append(f.read())
     return sep.join(buf)
 
-long_description = read('README.rst')
+long_description = read('README.md')
 
 """
 class PyTest(TestCommand):
